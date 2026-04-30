fbb0d71eeb7593759b32e4445dddb5f99f12f6fe
unknown <abdurrahmanaikon@gmail.com>
2026-04-30 13:26:10 +0700

    perbaikan index .js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,6 @@
-import { app } from '../backend/src/app';
+const app = require('../backend/src/app');
 
-export default app;
+module.exports = (req, res) => {
+  req.url = req.url.replace(/^\/api/, '');
+  return app(req, res);
+};
